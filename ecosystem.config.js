f69d401f7811dfb95f9984985de1e28fb84f1a87
DMITRY GORDEEV <gordeev.du@gmail.com>
2026-09-21 19:47:23 -1000

    fix: environment start order

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   apps: [
     {
+      name: "hcamp-backend",
+      cwd: "/var/www/apps/strapi",
+      script: "npm",
+      args: "run start",
+    },
+    {
       name: "hcamp-frontend",
       cwd: "/var/www/apps/ui",
       script: ".next/standalone/apps/ui/server.js",
@@ -8,12 +14,6 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 3000,
       },
-    },
-    {
-      name: "hcamp-backend",
-      cwd: "/var/www/apps/strapi",
-      script: "npm",
-      args: "run start",
     },
   ],
 }
